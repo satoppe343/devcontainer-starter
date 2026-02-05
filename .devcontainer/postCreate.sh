fdcea162990b39fe235f7f32f9f325d7d3b266cf
satoppe343 <satoppe343@gmail.com>
2026-02-05 13:29:42 +0000

    フォントファイルをインターネットからダウンロードする方式に変更

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 my_dir=$(dirname $0)
-cd ${my_dir}
 
 # oh-my-zshインストール及び設定を実行
-./sh/install-ohmyzsh.sh
+${my_dir}/sh/install-ohmyzsh.sh
+
+
+#その他
+# npm install -g npm
+# npm install -g pnpm
+# pnpm install
+# pnpm dev
