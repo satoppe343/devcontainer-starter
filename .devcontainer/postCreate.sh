68f98ef9ceeeb52ec7ddd663d6c5db66ec557527
satoppe343 <satoppe343@gmail.com>
2026-02-18 21:38:18 +0900

    細かな修正

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 my_dir=$(dirname $0)
+#cd ${my_dir}
 
 # oh-my-zshインストール及び設定を実行
 ${my_dir}/sh/install-ohmyzsh.sh
 
-
-#その他
-# npm install -g npm
-# npm install -g pnpm
 # pnpm install
-# pnpm dev
+# sudo npm install -g npm
+# sudo npm install -g pnpm
+
+# install packages
+# cd react
+# pnpm install
